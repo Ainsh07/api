9da44669443891defd0c27338a4910d03a4dd9c5
slickben <benson.isaac.momodu@gmail.com>
2022-09-08 15:14:44 +0100

    ....

--- hotel.js
+++ hotel.js
@@ -1,33 +1,74 @@
+
 let searchBtn = document.getElementById("search-btn");
-let countryInp = document.getElementsByClassName("search");
+let countryInp = document.getElementById("search-input");
 searchBtn.addEventListener("click", ()=> {
-    let countryName = countryInp.value;
-    const options = {
+    let countryName = countryInp.value ;
+   const options = {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': '23c3877b72msh7fbd8995e46cbedp155a90jsn9f6f2ac20b08',
             'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
         }
     };
+    if (countryName == ""){
+        alert(`Enter a country name...`)
+    }
+    else{
     
-    fetch('https://hotels4.p.rapidapi.com/properties/get-hotel-photos?id=1178275040', options)
+    fetch(`https://hotels4.p.rapidapi.com/locations/v2/search?query=${countryName}&locale=en_US&currency=USD`, options)
         .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-   
-    // .then(data =>{
-    //     console.log(data)
-    //     const hotelList = document.querySelector('.hotelList')
+        // .then(response => console.log(response))
+        // .catch(err => console.error(err));
+        
+        // checking data
+        .then(data => {
+            // console.log(typeof data)
+            const hotelList = document.querySelector('.hotel-list')
+            // extract all entities in the suggested array
+            const allSuggestedEntities = data.suggestions.map( item => item.entities)
 
-    //     let suggestions = data.suggestions
-    //     suggestions.forEach(suggestion => {
-    //         var z = document.createElement('p');
-    //         z.innerHTML = suggestion.group;
-    //         hotelList.appendChild(z)
-         
-    //     });
-    // })
-    })
+            // concatinate it
+            // using array method flat, please do well to read up on flat 
+            let concatinatedArray = allSuggestedEntities.flat()
+
+            // use this to display numbers of hotel found
+            document.getElementById("count").innerText = `${concatinatedArray.length} + Hotels found `
+
+            // container to hold up the list of hotel
+            let div = ''
+            // loop throught the concatinated array and add it up using the div variable created obeve
+            concatinatedArray.forEach( entity => {
+                // add to the list of hotel
+                div += `
+                    <div class="hotel1">
+                        <img id="hotel1" src="main/images/Blog Img 1.png">
+                        <h3>${entity.caption}</h3>
+                        <h4> ${entity.name}</h4>
+                        <p> ${entity.type}</p>
+                        <p> ${entity.destinationId}</p>
+                    </div>`
+            })
+            
+            document.getElementById("hotel-list").innerHTML = div
+            
+        })
+    }
+    });
+
+    // toggle-------------
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+    
+    hamburger.addEventListener('click', () =>{
+        hamburger.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    });
+    
+    document.querySelectorAll('.nav-link').forEach(n =>
+        n.addEventListener("click", () =>{
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
+    }));
 
 
     
